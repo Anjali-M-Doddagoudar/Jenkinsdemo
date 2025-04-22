8dd8f9d1daba07646fd594478aa4f60653ef161a
Anjali-M-Doddagoudar <95429986+Anjali-M-Doddagoudar@users.noreply.github.com>
2025-04-22 16:11:26 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -6,12 +6,14 @@ public class Employee {
         int id = 56;
         int salary = 20000;
         String company = "ANZ";
+        long phoneno = 864900303;
 
         System.out.println("<-------Employee Details------->");
         System.out.println("Employee name:" +name);
         System.out.println("Employee id:" +id);
         System.out.println("Employee salary:" +salary);
         System.out.println("Employee company:" +company);
+        System.out.println("Employee phone number:" +phoneno);
     }
     
 }
