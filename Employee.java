f9fb44c9ef5a2a0b8f5f3adec4f50273afc38f20
deepajadhav110 <deepajadhav2001@gmail.com>
2025-04-22 13:56:14 +0530

    Save the file in main

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+
+    public static void main(String[] args) {
+        
+        String name = "xyz";
+        int id = 56;
+        int salary = 20000;
+        String company = "ANZ";
+
+        System.out.println("<-------Employee Details------->");
+        System.out.println("Employee name:" +name);
+        System.out.println("Employee id:" +id);
+        System.out.println("Employee salary:" +salary);
+        System.out.println("Employee company:" +company);
+    }
+    
+}
